077d48e078ebff71b2d6398b22c8360702a6dfb5
DinosaurPower <maiialysytska@gmail.com>
2026-04-04 19:42:54 -0400

    making npc animations

---
+++ Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class AnimationManager : MonoBehaviour
+{
+    public Animator MovingInAndOut;
+    public Animator FabricMotions;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void TravelIn()
+    {
+        
+    }
+}
