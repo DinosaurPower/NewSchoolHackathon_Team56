9b79206645d030e038c3b47e45fb5bf53bcf0cf1
DinosaurPower <maiialysytska@gmail.com>
2026-04-04 12:29:32 -0400

    Added scripts for game objects, npcs, and interaction manager

---
+++ Assets/Scripts/NPCInfo.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class NPCInfo : MonoBehaviour
+{
+    public string Name;
+    public StoryObject[] LostPossessions;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
